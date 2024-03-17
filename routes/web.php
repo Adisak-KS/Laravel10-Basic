e066f939b3cc1f3ad1f9a94f537d86c2f7d6f76b
Adisak-KS <adisak.general@gmail.com>
2024-03-17 10:48:33 +0700

    Design With Bootstrap5

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 
 Route::get('about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('blog', function () {
     return view('blog');
-});
+})->name('blog');
 
