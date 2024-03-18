c0f1c8f2239e4974d63ca40e8f76f9506b1da1ec
Adisak-KS <adisak.general@gmail.com>
2024-03-18 16:02:24 +0700

    Update Status (Query Builder)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -52,4 +52,13 @@ class AdminController extends Controller
         DB::table('blogs')->where('id', $id)->delete();
         return redirect('/blog');
     }
+
+    function change($id){
+        $blog = DB::table('blogs')->where('id', $id)->first();
+        $data=[
+            'status'=>!$blog->status
+        ];
+        $blog=DB::table('blogs')->where('id', $id)->update($data);
+        return redirect('/blog');    
+    }
 }
