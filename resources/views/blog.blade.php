20f6bf331ea6305b3de22dcd34eadf998374095d
Adisak-KS <adisak.general@gmail.com>
2024-03-18 09:51:38 +0700

    Show data in table with boostrap5

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,29 @@
 @extends('layout')
 @section('title', 'บทความทั้งหมด')
 @section('content')
-    <h2>บทความทั้งหมด</h2>
-    <hr>
-    @foreach ($blogs as $item)
-        <h4>{{$item["title"]}}</h4>
-        <p>{{$item["content"]}}</p>
-        @if ($item["status"] == true)
-            <p class="text-success">เผนแพร่</p>
-        @else
-            <p class="text-danger">ฉบัยร่าง</p>
-        @endif
-        <hr>
-    @endforeach
+    <h2 class="text-center py-2">บทความทั้งหมด</h2>
+    <table class="table table-bordered text-center">
+        <thead>
+            <tr>
+                <th scope="col">ชื่อบทความ</th>
+                <th scope="col">เนื้อหา</th>
+                <th scope="col">สถานะ</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($blogs as $item)
+                <tr>
+                    <td>{{ $item['title'] }}</td>
+                    <td>{{ $item['content'] }}</td>
+                    <td>
+                        @if ($item['status'] == true)
+                            <a href="#" class="btn btn-success">เผยแพร่</a>
+                        @else
+                            <a href="#" class="btn btn-warning">ฉบับร่าง</a>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
